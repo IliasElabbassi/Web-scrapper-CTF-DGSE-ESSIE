ce830278d816cd496ae5fa3ee3aa37c05fa9c30f
Ilias El abbassi <38820888+IliasElabbassi@users.noreply.github.com>
2021-02-16 12:48:07 +0100

    Update scrapper_th.py

--- scrapper_th.py
+++ scrapper_th.py
@@ -24,17 +24,13 @@ def scrap(i, start, end):
 		url = "https://www.challengecybersec.fr/9bcb53d26eab7e9e08cc9ffae4396b48/blog/post/"
 
 		response = get(url+str(idx))
-
 		source = None
-
 		while response.status_code != 404:
 			if idx >= end:
 				break
-
 			source = response.text
 			selector = Selector(text=source)
 			digest = selector.xpath('//span/text()').extract()
-
 			try:
 				if digest[0]:
 					print(digest[0])
@@ -43,7 +39,6 @@ def scrap(i, start, end):
 				break
 
 			encryption[i] += str(digest[0])
-
 			idx += 1
 			print(str(idx)+" thread:"+str(i))
 			response = get(url+str(idx))
@@ -60,32 +55,24 @@ def scrap(i, start, end):
 #### threading ####
 start = 1
 end = 1000/20
-
 encryption = []
 
 for i in range(20):
 	encryption.append("") 
 
 threads = []
-
 for i in range(0,20):
 	t = Thread(target=scrap, args=[i, start, end])
-	
 	start += 50
 	end += 50
-
 	threads.append(t)
 	t.start()
-
 
 for thread in threads:
 	thread.join()
 
-
 #### encryption
-
 toEncrypt = ""
-
 for s in encryption:
 	toEncrypt += str(s)
 
@@ -103,8 +90,6 @@ out.write("\n")
 out.write("digest : \n")
 out.write(str(res.digest()))
 
-
 ### stop timer
 stop = timeit.default_timer()
-
 print("time: ", stop-start_timer)
